331443719078edad533a096fce43b3bdf81d2073
unknown <sudharsansrikanthan@gmail.com>
2022-02-19 19:12:58 +0530

    Modifications to IdentityRecord, CredentialSchema and CredentialDefinition contracts

--- contracts/IdentityRecord.sol
+++ contracts/IdentityRecord.sol
@@ -9,22 +9,32 @@ import "./Roles.sol";
 contract IdentityRecord is Roles {
 
     struct Record {
-        int256 DID_public;
+        string DID_public;
         bytes32 Vkey;
         string Url;
     }
     
     mapping(address => Record) identityRecord;
+    mapping(string => bool) DID_public_exists;
 
-    function Verinym(address _address,int256 _DID_public, bytes32 _Vkey, string memory _Url) internal onlyStewardOrTrustAnchor {
+    function setVerinym(address _address,string memory _DID_public, bytes32 _Vkey, string memory _Url) internal onlyStewardOrTrustAnchor {
+
+        //checking whether the given address already have a Verinym or not
+        require(entityRegistry[_address].role == Role.User,"Given address is already a trust anchor");
+
+        // checking whether the given Public DID already exist or not
+        require(!DID_public_exists[_DID_public], "This Public DID already exists");
 
         identityRecord[_address].DID_public = _DID_public;
         identityRecord[_address].Vkey = _Vkey;
         identityRecord[_address].Url = _Url;
 
         assignTrustAnchorRole(_address); // Gets the Trust anchor role as it now have a Public DID
+        DID_public_exists[_DID_public] = true;
 
     }
               
-
+    function getVerinym(address _address) internal view returns(Record memory) {
+        return identityRecord[_address];
+    }
 }
