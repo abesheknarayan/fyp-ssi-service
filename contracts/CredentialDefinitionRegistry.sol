f5ac4670abda41930ef4458d051a742c816d45bf
unknown <sudharsansrikanthan@gmail.com>
2022-02-19 19:12:58 +0530

    Modifications to IdentityRecord, CredentialSchema and CredentialDefinition contracts

--- contracts/CredentialDefinitionRegistry.sol
+++ contracts/CredentialDefinitionRegistry.sol
@@ -1,16 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import './EntityRegistry.sol';
+import './Roles.sol';
+import './CredentialSchemaRegistry.sol';
 
-// Registry for storing credential schemas
-contract CredentialDefinitionRegistry is EntityRegistry {
+// Registry for storing credential definition
+contract CredentialDefinitionRegistry is Roles, CredentialSchemaRegistry {
 
     struct CredentialDefinition {
-        Entity owner;
+        string name;
+        bytes32 id;
+        string version;
+        address issuer_address;
+        bytes32 Vkey; // verification key for this particular credential definition.
+
+        bytes32 credSchema_id; // the attributes which the credential provides based on this definition can be looked up in the corresponding schema
+
+        bool isRevocatable; // whether the credential issued based on this credential definition could be revocated or not
+        // bytes32 revocation_registry_id; // revocation registry id for this particular credential definition
     }
 
-    // mapping of credential schema hash to hash of address of issuer (required in accumulator modifier checking)
+    // mapping of credential definition hash to hash of address of issuer (required in accumulator modifier checking)
     mapping(bytes32  => bytes32) credential_definition_hash_to_issuer_entity_address_hash;
+
+    mapping(address => CredentialDefinition[]) allCredentialDefinition; // all credential definition published by a particular trust anchor
+    mapping(bytes32 => CredentialDefinition) credentialDefinition;   // particular credential definition
+    mapping(bytes32 => bool) credential_definition_exists;   // whether a particular credential definition exists or not
+
+    function setCredentialDefinition(string memory _name, string memory _version,
+        bytes32 _Vkey, bytes32 _credSchema_id, bool _isRevocatable) internal onlyStewardOrTrustAnchor{
+
+            // already existing credential definition shouldn't be modified
+            bytes32 _id = keccak256(abi.encodePacked(_name,_version,tx.origin));
+            require(!credential_definition_exists[_id],"Existing credential definition can't be modified");
+
+            // checking whether the given id correspond to a credential schema
+            require(credential_schema_exists[_credSchema_id],"The credential schema referred does not exist");
+
+            CredentialDefinition memory new_cred_definition = CredentialDefinition(_name,_id,_version,tx.origin,_Vkey,_credSchema_id,_isRevocatable);
+            credentialDefinition[_id] = new_cred_definition;
+            credential_definition_exists[_id] = true;
+            allCredentialDefinition[tx.origin].push(new_cred_definition);
+    }
+
+    // if credential definition id is not known this function could be used
+    function getCredentialDefinition(string memory _name, string memory _version, address _address) internal view returns(CredentialDefinition memory) {
+        
+        bytes32 _id = keccak256(abi.encodePacked(_name,_version,_address));
+        
+        // checking whether the given details correspond to a credential definition
+        require(credential_definition_exists[_id],"Requested credential definition does not exist");
+
+        return credentialDefinition[_id];
+    }
+
+    // if credential definition id is known then this function could be used
+    function getCredentialDefinitionWithID(bytes32 _id) internal view returns(CredentialDefinition memory) {
+        
+        // checking whether the given id correspond to a credential definition
+        require(credential_definition_exists[_id],"Requested credential definition does not exist");
+
+        return credentialDefinition[_id];
+    }
+
+    function getAllCredentialDefinition(address _address) internal view returns(CredentialDefinition[] memory) {
+
+        // checking whether the given address has atleast one credential definition
+        require(allCredentialDefinition[_address].length >= 1,"No credential definition is published by this address");
+
+        return allCredentialDefinition[_address];
+    }
 
 }
--- contracts/CredentialSchemaRegistry.sol
+++ contracts/CredentialSchemaRegistry.sol
@@ -8,24 +8,54 @@ contract CredentialSchemaRegistry is Roles {
 
     struct CredentialSchema {
         string name;
-        uint id;       // uniqueness
-        address creator_address;  // modify
+        bytes32 id;
+        string version;
+        address creator_address;
         string attributes; 
     }
 
-    mapping(address => mapping(uint => CredentialSchema)) credentialSchema;
+    mapping(address => CredentialSchema[]) allCredentialSchema;   // all credential schemas published by a particular trust anchor
+    mapping(bytes32 => CredentialSchema) credentialSchema;   // particular credential schema
+    mapping(bytes32 => bool) credential_schema_exists;   // whether a particular credential schema exists or not
 
-    function setCredentialSchema(string memory _name, uint _id, string memory _attributes) internal onlyStewardOrTrustAnchor {
+    function setCredentialSchema(string memory _name, string memory _version, string memory _attributes) internal onlyStewardOrTrustAnchor {
 
-        // already existing schema shouldn't be updated
+        // already existing schema shouldn't be modified
+        bytes32 _id = keccak256(abi.encodePacked(_name,_version,tx.origin));
+        require(!credential_schema_exists[_id],"Existing Schema can't be modified");
 
-        CredentialSchema memory new_schema = CredentialSchema(_name,_id,tx.origin,_attributes);
-        credentialSchema[tx.origin][_id] = new_schema;
+        CredentialSchema memory new_schema = CredentialSchema(_name,_id,_version,tx.origin,_attributes);
+        credentialSchema[_id] = new_schema;
+        credential_schema_exists[_id] = true;
+        allCredentialSchema[tx.origin].push(new_schema);
     }
 
-    function getCredentialSchema(address _addr, uint _id) internal view returns(CredentialSchema memory){
+    // if credential schema id is not known this function could be used
+    function getCredentialSchema(string memory _name, string memory _version, address _address) internal view returns(CredentialSchema memory){
 
-        return credentialSchema[_addr][_id];
+        bytes32 _id = keccak256(abi.encodePacked(_name,_version,_address));
+
+        // checking whether the given details correspond to a credential schema
+        require(credential_schema_exists[_id],"Requested credential schema does not exist");
+
+        return credentialSchema[_id];
+    }
+
+    // if credential schema id is known then this function could be used
+    function getCredentialSchemaWithID(bytes32 _id) internal view returns(CredentialSchema memory){
+
+        // checking whether the given id correspond to a credential schema
+        require(credential_schema_exists[_id],"Requested credential schema does not exist");
+
+        return credentialSchema[_id];
+    }
+
+    // retriving all the credential schemas published by a particular turst anchor.
+    function getAllCredentialSchemas(address _address) internal view returns(CredentialSchema[] memory) {
+
+        // checking whether the given address has atleast one credential schema
+        require(allCredentialSchema[_address].length >= 1,"No credential schema is published by this address");
+
+        return allCredentialSchema[_address];
     }
 }
-
